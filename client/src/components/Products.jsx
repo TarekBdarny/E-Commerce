9e3adc3fd4e7677962d8e27090ef98984286f661
Tarek <tarekdevel41@gmail.com>
2024-10-18 00:40:52 +0300

    css

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 const Products = () => {
   return (
-    <div className="grid place-content-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-4 px-8 ">
+    <div className="grid place-content-center grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  py-4 px-8 ">
       {[1, 2, 3, 4].map((_, i) => (
         <Card key={i} />
       ))}
